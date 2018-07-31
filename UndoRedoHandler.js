bd68072a51e848187bae6917eabfcee8a5275b1c
Lam Pham <completejavascript.super@gmail.com>
2018-07-31 08:51:56 +0700

    Create UndoRedoHandler.js

---
+++ UndoRedoHandler.js
@@ -0,0 +1,34 @@
+class UndoRedoHandler {
+  constructor(currentstate) {
+    this._undoStack = [];
+    this._redoStack = [];
+    this._redoStack.push(currentstate);
+  }
+ 
+  insert(state) {
+    this._undoStack.push(this._redoStack.pop());
+    this._redoStack.length = 0;
+    this._redoStack.push(state);
+  }
+  
+  getPrevState() {
+    if (this._undoStack.length >= 1) {
+      let state = this._undoStack.pop();
+      this._redoStack.push(state);
+      return state;
+    }
+  }
+  
+  getNextState() {
+    if (this._redoStack.length >= 2) {
+      let state = this._redoStack.pop();
+      this._undoStack.push(state);
+      return this._redoStack[this._redoStack.length - 1];
+    }
+  }
+  
+  reset() {
+    this._undoStack.length = 0;
+    this._redoStack.length = 0;
+  }
+}
